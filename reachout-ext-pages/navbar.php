30e41bae221eb0f84c49f65e84c718aa2afb5959
Jordan Bamford <jordanbamford@jordans-mbp.home>
2020-04-25 10:08:50 +0100

    Slight changes to external public site front page and adding files

---
+++ reachout-ext-pages/navbar.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col-md-12">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<a class="navbar-brand" href="#"><img class="brand-img" src="images/reachout-logo-dark.png" /></a>
+				<!-- <ul class="navbar-nav">					
+					<li class="nav-item"><a class="nav-link" href="blog">Blog</a></li>
+				</ul> -->
+			</div>
+		</nav>
+	</div>
+</div>
---
+++ reachout-ext-pages/reachoutsite.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+	<title>ReachOut</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+		crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+		crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+		crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css/cc.css" />
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+	<div class="container-fluid">
+
+		<div class="row">
+			<div class="col-md-12">
+				<nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
+					<button class="navbar-toggler" type="button" data-toggle="collapse"
+						data-target="#bs-example-navbar-collapse-1">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<a class="navbar-brand" href="#"><img class="brand-img" alt="ReachOut Logo" src="images/reachout-logo-dark.png" /></a>
+						<!-- <ul class="navbar-nav">					
+							<li class="nav-item"><a class="nav-link" href="blog">Blog</a></li>
+						</ul> -->
+					</div>
+				</nav>
+			</div>
+		</div>
+
+		<div class="jumbotron">
+			<div class="row">
+				<img src="images/reachout.png" alt="reachout logo" class="img-circle img-responsive center">
+			</div>
+			<div class="container flex-center">
+				<div class="row pt-5 mt-3">
+					<div class="col-lg-6 mb-5 text-center text-lg-left">
+						<h1 class="h1-responsive font-weight-bold">Vulnerable, Self-Isolating or ready to provide help?</h1>
+						<hr class="hr-light">
+						<p>ReachOut will provide a platform where you can request services from others, or help those local to you that are
+							vulnerable or affected by COVID-19. Help can be requested for many things, so you can get the support you need or
+							provide help to those that need it. Once verified, you can make requests or start helping others immediately. We
+							offer many different ways to help so even if you are vulnerable you may still be able to get involved remotely.</p>
+						<br>
+					</div>
+
+					<div class="col-lg-6">
+						<div class="embed-responsive embed-responsive-16by9">
+							<video width="320" height="240" controls>
+								<source src="images/reachoutpitch.mp4" type="video/mp4">
+							</video>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row" class="midpage">
+				<div class="col-md-4">
+					<img src="images/guidelines.jfif" alt="gov guidelines" class="homeimage">
+					<h2>In Self-Isolation or Vulnerable?</h2>
+					<p>If you are in need of services then you will be able to sign up and provide us with information about your current
+						condition. You can then make requests for any essential items such as collection and drop-off for food or
+						medication.</p>
+				</div>
+				<div class="col-md-4">
+					<img src="images/food.jfif" alt="food" class="homeimage">
+					<h2>In Need Of Help?</h2>
+					<p>We are happy to create the connection between those in isolation, and those willing to assist with things like food
+						shopping, dog walking, plant watering, so you can get what you need, when you need it.</p>
+				</div>
+				<div class="col-md-4">
+					<img src="images/phonecall.jfif" alt="phonecall" class="homeimage">
+					<h2>Just Want a Conversation?</h2>
+					<p>Being in self-isolation can lead to loneliness for many people. We aim to prevent this by allowing users to connect
+						over the phone to speak when they may have nobody else to listen.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
