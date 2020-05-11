4e0a4fb42d6e7ff0cf7f6e6680b8bf8720d40ded
BellJohn <e.jonathan.a.bell@hotmail.com>
2020-05-11 02:32:31 +0100

    Missed a line

--- reachout/src/main/java/com/reachout/dao/HibernateRequestDAOImpl.java
+++ reachout/src/main/java/com/reachout/dao/HibernateRequestDAOImpl.java
@@ -78,6 +78,7 @@ public class HibernateRequestDAOImpl extends HibernateListingDAOImpl {
 			session.delete(request);
 			Query query = session.createNativeQuery("DELETE FROM ASSIGNED_LISTINGS WHERE AS_LISTING_ID = :lst_id");
 			query.setParameter("lst_id", request.getId());
+			query.executeUpdate();
 			session.flush();
 			session.getTransaction().commit();
 		} catch (IllegalStateException | RollbackException e) {
